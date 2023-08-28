4468ce79c40b0a20fa018acc085aa19d7be06baa
akhashi <andersonfelpe123@gmail.com>
2023-08-27 22:30:01 -0300

    [+]: add suport to trimui sdl

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,10 @@ FROM debian:bullseye
 RUN apt-get update && apt-get install -y \
     g++-arm-linux-gnueabihf \
     qemu-user \
+    zsh \
     cmake \
     make \
+    wget \
     && rm -rf /var/lib/apt/lists/*
 
 # Configure o diretório de trabalho
@@ -15,5 +17,14 @@ WORKDIR /app
 # Copie o código-fonte para o contêiner
 COPY SOURCE_CODE /app/SOURCE_CODE
 
+# Baixe e compile o SDL2
+RUN wget https://www.libsdl.org/release/SDL2-2.0.14.tar.gz && \
+    tar -xzvf SDL2-2.0.14.tar.gz && \
+    cd SDL2-2.0.14 && \
+    ./configure --host=arm-linux-gnueabihf --prefix=/usr && \
+    make && \
+    make install
+
 # Defina o comando padrão que será executado quando o contêiner for iniciado
-CMD ["bash"]
+CMD ["zsh"]
+
--- SOURCE_CODE/srcs/homebrew.cpp
+++ SOURCE_CODE/srcs/homebrew.cpp
@@ -1,4 +1,5 @@
 #include "../includes/homebrew.hpp"
+#include <SDL2/SDL_video.h>
 
 using namespace std;
 
@@ -15,6 +16,19 @@ CheckArguments
 int
 main(int ac, char **av) {
   (void)av;
+  if (SDL_Init(SDL_INIT_VIDEO) != 0){
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    return 1;
+  }
+  SDL_Window *window = SDL_CreateWindow("SDK Example", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 320, 240, 0);
+  if (!window){
+    SDL_Log("Unable to create SDL: %s", SDL_GetError());
+    return -1;
+  }
+
+  SDL_Delay(3000);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   if (ac < 2) {
   	cout << "No input sent, Check docs -h or --help" << endl;
   	return -1;
@@ -22,3 +36,25 @@ main(int ac, char **av) {
   cout << "Hello, World!" << endl;
   return 0;
 }
+/*
+#include <SDL2/SDL.h>
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    if (!window) {
+        SDL_Log("Unable to create window: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}*/
